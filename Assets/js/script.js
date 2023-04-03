31e0a39c07f849898686d2c5135c4c7d45c70f29
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-03 16:35:26 -0700

    updated

---
+++ Assets/js/script.js
@@ -0,0 +1,67 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var special = "@#$%^&*";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+
+function generatePassword() {
+
+   var numCharacter = prompt("How much number of characters you want between 8 and 128?");
+
+   if (numCharacter < 8 || numCharacter > 128) {
+      alert("The password should be 8 to 128")
+      generatePassword()
+
+   }
+
+
+   var numbers = confirm("Do you want to use numbers in your password?");
+   var uppercases = confirm("Do you want to use Uppercases in your password?");
+   var lowercases = confirm("Do you want to use lowercases in your password?");
+   var symbols = confirm("Do you want to use symbols in your password?");
+
+   // charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*()@~:;,./?{}=-_",
+
+   //  retVal = "";
+
+   lastPassword = "";
+
+   //   for (var i = 0, n = charset.length; i < numCharacter; ++i) {
+   for (var i = 0; i < length; i++) {
+      if (uppercases === true && lastPassword.length < length) {
+         var select = Math.floor(Math.random() * 26);
+         lastPassword = lastPassword + upperCase.charAt(select);
+      }
+      if (lowercases === true && lastPassword.length < length) {
+         var select = Math.floor(Math.random() * 26);
+         lastPassword = lastPassword + lowerCase.charAt(select);
+
+         // retVal += charset.charAt(Math.floor(Math.random() * n));
+      }
+      if (numbers === true && lastPassword.length < length) {
+         var select = Math.floor(Math.random() * 26);
+         lastPassword = lastPassword + number.charAt(select);
+         //   return retVal
+      }
+      if (symbols === true && lastPassword.length < length) {
+         var select = Math.floor(Math.random() * 26);
+         lastPassword = lastPassword + special.charAt(select);
+      }
+   }
+   return lastPassword
+
+}
+
+// Write password to the #password input
+function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+
+   passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# pass-test
+Password-Generator-Homework-3
+Password Generator Homework 3 - UCF Coding BootCamp
+This project was designed as a homework assignment week 3.
+
+This application emphasizes the use of Javascript to generate a random, secure password for the user.
+
+Getting Started
+This code was made it in JavaScript to create random password with number, special character, lowercase and uppercase result.
+
+Components in this repo:
+HTML
+CSS
+Javascript
+README
+
+
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+Check my work - Click!
